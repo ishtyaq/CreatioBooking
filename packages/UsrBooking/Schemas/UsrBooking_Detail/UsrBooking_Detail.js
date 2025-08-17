c9f674ec4e6e05ef05b700db0c3360243f073f20
ishtiaq.Ahmad <ishtiaq.ahmad.c@rga.gov.sa>
2025-08-17 12:10:02 +0400

    Booking Object added

---
+++ packages/UsrBooking/Schemas/UsrBooking_Detail/UsrBooking_Detail.js
@@ -0,0 +1,8 @@
+define('UsrBooking_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrBooking',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
